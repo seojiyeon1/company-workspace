dcf6bf25a5e4068588e3b99a3a13803479b6e35c
seojiyeon1 <jy20001201@gmail.com>
2025-03-05 17:00:59 +0900

    ch1-5 하단 바

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 
 import { BrowserRouter } from "react-router-dom";
 
@@ -8,6 +9,8 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      
+      <Footer />
     </BrowserRouter>
   )
 }
