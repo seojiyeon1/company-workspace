8c1453b6804326fc218afeda999df6c2b4a6999b
seojiyeon1 <jy20001201@gmail.com>
2025-03-24 14:26:18 +0900

    ch4-2 MongoDB와 Express 서버 연동

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
-})
+  res.send("Hello world");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log(" MongoDB 연결 성공"))
+  .catch((error) => console.log("MongoDB 연결 실패", error));
 
 app.listen(PORT, () => {
-    console.log("Server is running");
-})
+  console.log("Server is running");
+});
